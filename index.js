e166530d04ac3f294655378a7d2c658c8f2a51b5
lilyan <lilyan.bastien@gmail.com>
2021-12-05 21:33:58 +0100

    mis au propre

--- index.js
+++ index.js
@@ -3,30 +3,39 @@ const app = new express()
 const { isValidObjectId } = require('mongoose');
 const modele = require('./models/Post');
 
+
+//EJS
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
+
 //import routes
 const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
 
+/*
 //post
-/*var path = require('path')
+var path = require('path')
 var bodyParser = require('body-parser');
-app.post('/', function(req,res){
+app.post('/views/index.ejs', function(req,res){
     console.log('receiving data ...');
     console.log('body is ',req.body);
     res.send(req.body);
-});*/
+});
+*/
+
+
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
-
+/*
 app.post('/', function(req, res) {
     console.log('post recu');
     console.log(req.body.IdTx);
     res.redirect('posts/' + req.body.IdTx);
 });
-
+*/
 //connect to DB
 const mongoose = require('mongoose');
 const { exit } = require('process');
@@ -36,26 +45,23 @@ mongoose.connect('mongodb+srv://design:design@cluster0.d5kff.mongodb.net/myFirst
     console.log(error);
 })
 
-app.use(express.static('dist'))
+app.use(express.static('public'))
 
 app.listen(4000, ()=>{
     console.log('App listening on port 4000')
 })
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'dist/index.html'))
+    //res.sendFile(path.resolve(__dirname, 'dist/index.html'))
+    res.render('index') ;
     })
 
 app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'dist/about.html'))
+    //res.sendFile(path.resolve(__dirname, 'dist/about.html'))
+    res.render('about') ;
     })
 
 app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'dist/contact.html'))
+    //res.sendFile(path.resolve(__dirname, 'dist/contact.html'))
+    res.render('contact') ;
     })
-
-app.get('/post',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'dist/post.html'))
-    })
-
-
